3cd0c3d2bef57a10fc71cc282f2cccffb51092a3
nihalshahria <nihalshahria@gmail.com>
2022-02-19 12:55:16 +0600

    question sorting added

--- backend/controllers/qnaControllers.js
+++ backend/controllers/qnaControllers.js
@@ -12,124 +12,149 @@ const mongoose = require("mongoose");
 //   )
 // );
 
-exports.getAllQuestion = async (req, res, next) => {
-  const { category } = req.query;
-
-  if (!category) {
-    const questions = await Question.find().populate();
-
-    res.status(200).json({
-      message: "successful",
-      No_of_questions: questions.length,
-      data: {
-        questions,
-      },
-    });
-  } else {
-    try {
-      const questions = await Question.find({
-        questionCategory: category,
-      }).populate();
-
-      res.status(200).json({
-        message: "successful",
-        No_of_questions: questions.length,
-        data: {
-          questions,
-        },
-      });
-    } catch (error) {
-      return next(new HttpError("Something Went Wrong", 401));
+const sort = (questions, sortBy) => {
+    let filteredQuestionsSorted;
+    if (sortBy === `dateAsc`) {
+        filteredQuestionsSorted = questions.sort((a, b) => {
+            return Number(a.createdAt) - Number(b.createdAt)
+        })
+    } else if (sortBy === `dateDesc`) {
+        filteredQuestionsSorted = questions.sort((a, b) => {
+            return Number(b.createdAt) - Number(a.createdAt)
+        })
+    }else if (sortBy === `ansAsc`) {
+        filteredQuestionsSorted = questions.sort((a, b) => {
+            return Number(a._answersId.length) - Number(b._answersId.length)
+        })
+    } else if (sortBy === `ansDesc`) {
+        filteredQuestionsSorted = questions.sort((a, b) => {
+            return Number(b._answersId.length) - Number(a._answersId.length)
+        })
     }
-  }
+    return filteredQuestionsSorted;
+};
+
+
+exports.getAllQuestion = async (req, res, next) => {
+    let { category, sortBy } = req.query;
+    if (!sortBy) {
+        sortBy = "dateDesc";
+    }
+    if (!category) {
+        let questions = await Question.find().populate();
+        questions = sort(questions, sortBy);
+        res.status(200).json({
+            message: "successful",
+            No_of_questions: questions.length,
+            data: {
+                questions,
+            },
+        });
+    } else {
+        try {
+            let questions = await Question.find({
+                questionCategory: category,
+            }).populate();
+            questions = sort(questions, sortBy);
+            res.status(200).json({
+                message: "successful",
+                No_of_questions: questions.length,
+                data: {
+                    questions,
+                },
+            });
+        } catch (error) {
+            return next(new HttpError("Something Went Wrong", 401));
+        }
+    }
 };
 
 exports.getQuestionByCategory = async (req, res, next) => {
-  const { category } = req.query;
+    const { category } = req.query;
 
-  if (!category) {
-    const questions = await Question.find().populate();
+    if (!category) {
+        const questions = await Question.find().populate();
 
-    res.status(200).json({
-      message: "successful",
-      No_of_questions: questions.length,
-      data: {
-        questions,
-      },
-    });
-  } else {
-    try {
-      const questions = await Question.find({ category: category }).populate();
-      res.status(200).json({
-        message: "successful",
-        No_of_questions: questions.length,
-        data: {
-          questions,
-        },
-      });
-    } catch (error) {
-      return next(new HttpError("Something Went Wrong", 401));
+        res.status(200).json({
+            message: "successful",
+            No_of_questions: questions.length,
+            data: {
+                questions,
+            },
+        });
+    } else {
+        try {
+            const questions = await Question.find({ category: category }).populate();
+            res.status(200).json({
+                message: "successful",
+                No_of_questions: questions.length,
+                data: {
+                    questions,
+                },
+            });
+        } catch (error) {
+            return next(new HttpError("Something Went Wrong", 401));
+        }
     }
-  }
 };
 
 exports.askAQuestion = async (req, res, next) => {
-  console.log(req.body);
-  const { questionTitle, questionBody, questionCategory, askedBy } = req.body;
-  const userId = req.userData.id;
+    console.log(req.body);
+    const { questionTitle, questionBody, questionCategory, askedBy } = req.body;
+    const userId = req.userData.id;
 
-  // const newQuestion = await Question.create({
-  //   questionTitle: questionTitle,
-  //   questionBody: questionBody,
-  //   questionCategory: questionCategory,
-  //   askedBy: askedBy,
-  //   _askerId: userId,
-  // });
-  const newQuestion = new Question({
-    questionTitle: questionTitle,
-    questionBody: questionBody,
-    questionCategory: questionCategory,
-    askedBy: askedBy,
-    _askerId: userId,
-  });
+    // const newQuestion = await Question.create({
+    //   questionTitle: questionTitle,
+    //   questionBody: questionBody,
+    //   questionCategory: questionCategory,
+    //   askedBy: askedBy,
+    //   _askerId: userId,
+    // });
+    const newQuestion = new Question({
+        questionTitle: questionTitle,
+        questionBody: questionBody,
+        questionCategory: questionCategory,
+        askedBy: askedBy,
+        _askerId: userId,
+    });
 
-  let patient;
-  try {
-    patient = await Patient.findById(userId);
-  } catch (err) {
-    const error = new HttpError(
-      "Failed to Ask Question , please try again",
-      500
-    );
-    return next(error);
-  }
-  if (!patient) {
-    const error = new HttpError("User Not Found", 404);
-    return next(error);
-  }
+    let patient;
+    try {
+        patient = await Patient.findById(userId);
+    } catch (err) {
+        const error = new HttpError(
+            "Failed to Ask Question , please try again",
+            500
+        );
+        return next(error);
+    }
+    if (!patient) {
+        const error = new HttpError("User Not Found", 404);
+        return next(error);
+    }
 
-  try {
-    const session = await mongoose.startSession();
-    session.startTransaction();
-    await newQuestion.save({ session: session });
-    patient.questions.push(newQuestion);
-    await patient.save({ session: session });
-    await session.commitTransaction();
-  } catch (err) {
-    console.log(err.message);
-    const error = new HttpError(
-      "Failed to Ask Question, please try again",
-      500
-    );
-    return next(error);
-  }
+    try {
+        const session = await mongoose.startSession();
+        session.startTransaction();
+        await newQuestion.save({ session: session });
+        patient.questions.push(newQuestion);
+        await patient.save({ session: session });
+        await session.commitTransaction();
+    } catch (err) {
+        console.log(err.message);
+        const error = new HttpError(
+            "Failed to Ask Question, please try again",
+            500
+        );
+        return next(error);
+    }
 
-  res.status(201).json({
-    message: "successful",
-    data: {
-      newQuestion,
-    },
-  });
+    res.status(201).json({
+        message: "successful",
+        data: {
+            newQuestion,
+        },
+    });
 };
 
 // {
@@ -137,99 +162,99 @@ exports.askAQuestion = async (req, res, next) => {
 //     select: "question _askerId",
 //     }
 exports.getQuestionsOfAnUser = async (req, res, next) => {
-  let user;
-  let id = req.params.id;
+    let user;
+    let id = req.params.id;
 
-  try {
-      user = await Patient.findById(id).populate("questions");
-  } catch (error) {
-    console.log(error.message);
-  }
+    try {
+        user = await Patient.findById(id).populate("questions");
+    } catch (error) {
+        console.log(error.message);
+    }
 
-  res.status(200).json({
-    message: "successful",
-    data: {
-      questions: user.questions.map((id) => id.toObject({ getters: true })),
-    },
-  });
+    res.status(200).json({
+        message: "successful",
+        data: {
+            questions: user.questions.map((id) => id.toObject({ getters: true })),
+        },
+    });
 };
 
 exports.getAQuestion = async (req, res, next) => {
-  const question = await Question.findById(req.params.id).populate({
-    path: "answers",
-    select: "answer answeredBy upvotes",
-  });
+    const question = await Question.findById(req.params.id).populate({
+        path: "answers",
+        select: "answer answeredBy upvotes",
+    });
 
-  res.status(200).json({
-    message: "successful",
-    data: {
-      question,
-      id: req.userData.id,
-    },
-  });
+    res.status(200).json({
+        message: "successful",
+        data: {
+            question,
+            id: req.userData.id,
+        },
+    });
 };
 
 exports.getAnAnswer = async (req, res, next) => {
-  const answer = await Answer.findById(req.params.id);
+    const answer = await Answer.findById(req.params.id);
 
-  res.status(200).json({
-    message: "successful",
-    data: {
-      answer,
-    },
-  });
+    res.status(200).json({
+        message: "successful",
+        data: {
+            answer,
+        },
+    });
 };
 
 exports.answerQuestion = async (req, res, next) => {
-  if (req.userData.type === "Patient") {
-    return next(new AppError("Unauthorized", 400));
-  }
-  const doctor = await Doctor.findById(req.userData.id);
+    if (req.userData.type === "Patient") {
+        return next(new AppError("Unauthorized", 400));
+    }
+    const doctor = await Doctor.findById(req.userData.id);
 
-  const newAnswer = await Answer.create({
-    _questionId: req.params.id,
-    answer: req.body.answer,
-    answeredBy: doctor.id,
-    upvotes: [],
-  });
+    const newAnswer = await Answer.create({
+        _questionId: req.params.id,
+        answer: req.body.answer,
+        answeredBy: doctor.id,
+        upvotes: [],
+    });
 
-  res.status(201).json({
-    message: "successful",
-    data: {
-      newAnswer,
-    },
-  });
+    res.status(201).json({
+        message: "successful",
+        data: {
+            newAnswer,
+        },
+    });
 };
 
 exports.upvoteAnswer = async (req, res, next) => {
-  const ansId = req.params.id;
-  const id = req.userData.id;
-  console.log(ansId);
-  let answer;
-  try{
-       answer = await Answer.findById(ansId);
+    const ansId = req.params.id;
+    const id = req.userData.id;
+    console.log(ansId);
+    let answer;
+    try {
+        answer = await Answer.findById(ansId);
 
-  }catch(err){
-      console.log(err.message);
-  }
-  const isUpvoted = answer.upvotes.length && answer.upvotes.includes(id);
+    } catch (err) {
+        console.log(err.message);
+    }
+    const isUpvoted = answer.upvotes.length && answer.upvotes.includes(id);
 
-  let option;
-  if (!isUpvoted) {
-    option = "$addToSet";
-  } else option = "$pull";
-  let ans = await Answer.findByIdAndUpdate(
-    ansId,
-    {
-      [option]: { upvotes: id },
-    },
-    { new: true }
-  );
+    let option;
+    if (!isUpvoted) {
+        option = "$addToSet";
+    } else option = "$pull";
+    let ans = await Answer.findByIdAndUpdate(
+        ansId,
+        {
+            [option]: { upvotes: id },
+        },
+        { new: true }
+    );
 
-  res.status(201).json({
-    message: "success",
-    data: {
-      ans,
-    },
-  });
+    res.status(201).json({
+        message: "success",
+        data: {
+            ans,
+        },
+    });
 };
